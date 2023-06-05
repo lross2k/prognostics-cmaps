3f3f9f20ca0a8c0d6274e3048144aea5b9d0f263
lross2k <luisross2000@outlook.com>
2023-06-04 19:18:45 -0600

    Kinda finished obtaining the base results
    ~lross2k~

--- fitting.R
+++ fitting.R
@@ -9,8 +9,14 @@ Data_6 <- as.data.frame(ret[6])
 rm(ret)
 
 equationData <- read.csv2('equation.csv')
-equation <- quote(equationData[1,1] * x   + equationData[2,1] * x^2 + 
-                  equationData[3,1] * x^3 + equationData[4,1] * x^4)
+# equation <- quote(equationData[1,1] * x   + equationData[2,1] * x^2 + 
+#                   equationData[3,1] * x^3 + equationData[4,1] * x^4)
+
+# equation <- quote(9.869138e-01  * x   + 5.524573e-05 * x^2 + 
+#                     -3.327101e-08 * x^3 + 3.277860e-09 * x^4)
+
+equation <- quote(9.869138e-01  * x   + 5.524573e02 * x^2 + 
+                    -1.327101e-20 * x^3 + 3.277860e-06 * x^4)
 
 source('analysis.R')
 fitting <- list()
@@ -23,4 +29,4 @@ for (.x in c(1:260)) {
   fitting <- TestFitting(Data_6, .x, fitting, equation)
 }
 
-min(unlist(fitting))
+summary(unlist(fitting))
